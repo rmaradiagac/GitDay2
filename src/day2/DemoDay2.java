7b4c6896817b8ca973b6fdde0151ccd10f44d11c
rmara <rmara@RobertoPC>
2025-01-31 17:48:07 -0600

    added comments

--- src/day2/DemoDay2.java
+++ src/day2/DemoDay2.java
@@ -20,12 +20,12 @@ public class DemoDay2 {
 		System.out.println(a + b);
 		
 	}
-	
+	// divide code
 	public void divide (int a, int b) {
 		System.out.println(a % b);
 		
 	}
-	
+	// substract method
 	public void substract (int a, int b) {
 		System.out.println(a - b);
 		
