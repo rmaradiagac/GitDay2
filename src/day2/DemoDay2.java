c73ee973ea2a02b8507db0b104fff041f442cb0f
rmara <rmara@RobertoPC>
2025-01-31 17:46:22 -0600

    added divide and substract

--- src/day2/DemoDay2.java
+++ src/day2/DemoDay2.java
@@ -14,4 +14,21 @@ public class DemoDay2 {
 	}
 	
 	// comment from github
+	
+	
+	public void add (int a, int b) {
+		System.out.println(a + b);
+		
+	}
+	
+	public void divide (int a, int b) {
+		System.out.println(a % b);
+		
+	}
+	
+	public void substract (int a, int b) {
+		System.out.println(a - b);
+		
+	}
+	
 }
